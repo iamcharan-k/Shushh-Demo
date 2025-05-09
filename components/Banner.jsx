13184ff645268468720fefb5c56c37991fe6a81f
iamcharan-k <csai927@gmail.com>
2025-05-09 06:20:58 +0530

    first commit

---
+++ components/Banner.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
+      <Image
+        className="max-w-56"
+        src={assets.adidas_yeezy_350_oreo}
+        alt="adidas_yeezy_350_oreo"
+      />
+      <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
+        <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
+          Level Up Your Game
+        </h2>
+        <p className="max-w-[343px] font-medium text-gray-800/60">
+          From Dripping looks to Royal Glow
+        </p>
+        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
+          Buy now
+          <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon_white} alt="arrow_icon_white" />
+        </button>
+      </div>
+      <Image
+        className="hidden md:block max-w-80"
+        src={assets.nike_joran_1_dior}
+        alt="nike_joran_1_dior"
+      />
+      <Image
+        className="md:hidden"
+        src={assets.nike_joran_1_low_unc}
+        alt="nike_joran_1_low_unc"
+      />
+    </div>
+  );
+};
+
+export default Banner;
